3fc8d9fd0736b17104f24f98dd17de27e140af49
Andey Laptev <laptewandrew@gmail.com>
2024-05-21 11:05:44 +0300

    add mobile menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const burgerBtn = document.querySelector(".header__burger");
+const headerMenu = document.querySelector(".header__menu");
 
 burgerBtn.addEventListener("click", () => {
     burgerBtn.classList.toggle("header__burger--active")
+    headerMenu.classList.toggle("header__menu--active")
 })
